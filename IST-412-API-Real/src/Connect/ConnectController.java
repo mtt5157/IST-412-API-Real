092883e94800c8a39cde24ee8cbec267e3fcb1b1
Morgan Radic <radic44morgan@gmail.com>
2018-03-18 20:42:08 -0400

    Morgan: Connect Use Case

---
+++ IST-412-API-Real/src/Connect/ConnectController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Connect;
+
+/**
+ *
+ * @author morganradic
+ */
+public class ConnectController
+{
+    private boolean connected;
+    private ConnectModel model;
+    
+    // This method will return the battery status of the board
+    public void getBatteryStatus()
+    {
+        
+    }
+
+    /**
+     * @return the connected
+     */
+    public boolean isConnected()
+    {
+        return connected;
+    }
+
+   
+    
+    
+}
