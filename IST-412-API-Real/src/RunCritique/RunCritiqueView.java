0ca4f9b0659a2255f02321b263a4ed7b9a54aa1d
mattperon <32337827+mattperon@users.noreply.github.com>
2018-03-18 21:49:40 -0400

    run critique initial

---
+++ IST-412-API-Real/src/RunCritique/RunCritiqueView.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RunCritique;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Matt Peron
+ * This class will display feedback to the users previous runs.
+ */
+public class RunCritiqueView extends JPanel {
+    
+}
